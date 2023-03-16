f2ea9caa6bf239c0983dbef71b24d8c252376263
gaogee <gaoju_os@163.com>
2023-03-16 21:12:12 +0800

    Initial commit

---
+++ Example/GJAttributes/GJViewController.h
@@ -0,0 +1,13 @@
+//
+//  GJViewController.h
+//  GJAttributes
+//
+//  Created by gaogee on 03/16/2023.
+//  Copyright (c) 2023 gaogee. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GJViewController : UIViewController
+
+@end
